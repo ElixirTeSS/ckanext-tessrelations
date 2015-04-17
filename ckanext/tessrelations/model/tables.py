9b551635de7029b12ba68d2f971fb24b5e81c3a6
Milo Thurston <knirirr@gmail.com>
2015-04-17 16:19:04 +0100

    Table for events now available, should be able to access group and package.

--- ckanext/tessrelations/model/tables.py
+++ ckanext/tessrelations/model/tables.py
@@ -7,6 +7,7 @@ import logging
 from sqlalchemy import Table
 from sqlalchemy import Column
 from sqlalchemy import types
+from sqlalchemy import ForeignKey
 from sqlalchemy.engine.reflection import Inspector
 
 
@@ -20,11 +21,16 @@ log = logging.getLogger(__name__)
 
 material_event_table = None
 material_node_table = None
+tess_event_table = None
 
 def setup():
     log.debug("In the setup...")
     if material_event_table is None:
         define_tables()
+
+        if not tess_event_table.exists():
+            log.debug("Creating events table.")
+            tess_event_table.create()
 
         if not material_event_table.exists():
            log.debug("Creating material <-> event table.")
@@ -35,34 +41,50 @@ def setup():
             material_node_table.create()
 
 
+
+
 def define_tables():
     log.debug("Defining tables")
     global material_event_table
     global material_node_table
+    global tess_event_table
+    global tess_dataset_table
+    global tess_group_table
 
-    # It seems that CKAN stores datasets ("training materials") in the "package" table, with a uuid
-    # as the primary key. I don't know yet about events &c.
+    # Attempt to gain access to the group table
+    tess_group_table = Table('group',metadata,extend_existing=True)
+    mapper(TessGroup, tess_group_table)
+
+    tess_dataset_table = Table('package',metadata,extend_existing=True)
+    mapper(TessDataset, tess_dataset_table)
+
+
+    # First attempt at events, with minimal information.
+    tess_event_table = Table('tess_events', metadata,
+                             Column('id',types.UnicodeText, primary_key=True),
+                             Column('url', types.UnicodeText, default=u''),
+                             )
+
+    mapper(TessEvents,tess_event_table)
+
     material_event_table = Table('material_event', metadata,
                                 Column('id',types.UnicodeText, primary_key=True, default=make_uuid),
-                                Column('material_id', types.UnicodeText, default=u''),
-                                Column('event_id', types.UnicodeText, default=u''),
+                                Column('material_id', types.UnicodeText, ForeignKey('package.id')),
+                                Column('event_id', types.UnicodeText, ForeignKey('tess_events.id')),
                             )
+    # ForeignKey('harvest_object.id')
 
     mapper(TessMaterialEvent, material_event_table)
 
     material_node_table = Table('material_node', metadata,
                                 Column('id',types.UnicodeText, primary_key=True, default=make_uuid),
-                                Column('material_id', types.UnicodeText, default=u''),
-                                Column('node_id', types.UnicodeText, default=u''),
+                                Column('material_id', types.UnicodeText, ForeignKey('package.id')),
+                                Column('node_id', types.UnicodeText, ForeignKey('group.id')),
                             )
 
     mapper(TessMaterialNode, material_node_table)
 
-    # Attempt to gain access to the group table
-    #inspector = Inspector.from_engine(engine)
-    #columns = inspector.get_columns('group')
-    tess_group_table = Table('group',metadata,extend_existing=True)
-    mapper(TessGroup, tess_group_table)
+
 
 
 
@@ -81,4 +103,10 @@ class TessMaterialNode(TessRelation):
 class TessGroup(DomainObject):
     pass
 
+# Event table
+class TessEvents(DomainObject):
+    pass
 
+# Datasets, i.e. training materials
+class TessDataset(DomainObject):
+    pass
