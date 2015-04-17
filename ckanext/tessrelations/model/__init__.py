7d8979a88159e9bc045bc406fce26c6bcd5ee26e
Milo Thurston <knirirr@gmail.com>
2015-04-17 14:08:57 +0100

    Alternate table creation strategy doesn't work either.

--- ckanext/tessrelations/model/__init__.py
+++ ckanext/tessrelations/model/__init__.py
@@ -15,6 +15,10 @@ from ckan.model.domain_object import DomainObject
 
 log = logging.getLogger(__name__)
 
+# http://docs.sqlalchemy.org/en/rel_0_8/orm/extensions/declarative.html
+#from sqlalchemy.ext.declarative import declarative_base
+#Base = declarative_base()
+
 material_event_table = None
 material_node_table = None
 
@@ -22,13 +26,18 @@ def setup():
     if material_event_table is None:
         define_tables()
 
-        if not material_event_table.exists():
-            log.debug("Creating material <-> event table.")
-            material_event_table.create()
+        if not material_event_table: #.exists() # Blows up
+           log.debug("Creating material <-> event table.")
+           material_event_table.create()
 
-        if not material_node_table.exists():
+        if not material_node_table: #.exists() # Blows up
             log.debug("Creating material <-> node table.")
             material_node_table.create()
+    '''
+    if not TessMaterialEvent.__table__.exists():
+        log.debug("Creating material <-> event table.")
+        TessMaterialEvent.__table__.create()
+    '''
 
 
 def define_tables():
@@ -47,7 +56,7 @@ def define_tables():
     log.debug("Table: " + str(material_event_table))
 
     # Blows up here...
-    mapper(TessMaterialEvent, material_event_table)
+    #mapper(TessMaterialEvent, material_event_table)
 
     material_node_table = (Table, 'material_node', metadata,
                                 Column('id',types.UnicodeText, primary_key=True, default=make_uuid),
@@ -55,7 +64,7 @@ def define_tables():
                                 Column('node_id', types.UnicodeText, default=u''),
                             )
 
-    mapper(TessMaterialNode, material_node_table)
+    #mapper(TessMaterialNode, material_node_table)
 
 
 class TessRelation(DomainObject):
@@ -63,8 +72,24 @@ class TessRelation(DomainObject):
     # stuff here (e.g. for searching) later.
     pass
 
+
+
 class TessMaterialEvent(TessRelation):
     pass
 
 class TessMaterialNode(TessRelation):
     pass
+
+'''
+class TessMaterialEvent(Base):
+    __tablename__ = "materials_events"
+    id = Column('id',types.UnicodeText, primary_key=True, default=make_uuid),
+    material_id = Column(types.UnicodeText, primary_key=u'')
+    event_id = Column(types.UnicodeText, default=u'')
+
+class TessMaterialNode(Base):
+    __tablename__ = "materials_nodes"
+    id = Column(types.UnicodeText, primary_key=True, default=make_uuid),
+    material_id = Column(types.UnicodeText, default=u'')
+    node_id = Column(types.UnicodeText, default=u'')
+'''
